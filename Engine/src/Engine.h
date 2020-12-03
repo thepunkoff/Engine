0eaca27ae4aeed93a236fe4a5c5c91496f76a3f1
thepunkoff <thepunkoff@gmail.com>
2020-12-03 20:38:45 +0300

    Initial commit

---
+++ Engine/src/Engine.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by Engine applications
+
+#include "Engine/Application.h"
+
+// ---------- Entry point ----------
+#include "Engine/EntryPoint.h"
+// ---------------------------------
